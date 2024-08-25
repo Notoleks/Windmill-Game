cf60ae330e5d0b1958f4b4a4bc3d99cd358d9ba6
Notoleks <yuan8935@gmail.com>
2024-08-25 12:04:29 -0400

    First commit

---
+++ windmill.py
@@ -0,0 +1,49 @@
+import pygame
+from sys import exit
+import pygame_gui
+
+
+pygame.init()
+width, height = 1800,1000
+screen = pygame.display.set_mode((width,height))
+clock = pygame.time.Clock()
+
+manager = pygame_gui.UIManager((width, height))
+
+windmill = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 100), (150, 150)), text ='', manager=manager)
+windmill.set_image("Windmill1.png")
+clock = pygame.time.Clock()
+
+class Board:
+    def __init__(self, width, height, screen):
+        self.board = pygame.image.load("windmill_board.png")
+        self.rect = self.board.get_rect()
+        self.screen = screen
+        # self.board = pygame.transform.scale(self.board,(600, 600))
+        self.rect.center = (width/2, height/2)
+
+
+    def draw(self):
+        self.screen.blit(self.board, self.rect)
+
+
+board1 = Board(width, height, screen)
+while True:
+    time_delta = clock.tick(60)/1000.0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == windmill:
+                print('Windmill placed')
+        manager.process_events(event)
+    manager.update(time_delta)
+    board1.draw()
+    manager.draw_ui(screen)
+
+        #game code
+
+
+    pygame.display.update()
+    # clock.tick(60)
