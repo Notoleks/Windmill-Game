e46131a146d38ea243d55027763a163d517b6cb9
Notoleks <yuan8935@gmail.com>
2024-09-14 12:01:21 -0400

    removed pygame_gui

--- windmill.py
+++ windmill.py
@@ -1,6 +1,5 @@
 import pygame
 from sys import exit
-import pygame_gui
 
 
 pygame.init()
@@ -8,66 +7,60 @@ width, height = 1800,1000
 screen = pygame.display.set_mode((width,height))
 clock = pygame.time.Clock()
 time = pygame.time.get_ticks()
-manager = pygame_gui.UIManager((width, height))
 
-windmill = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 100), (150, 150)), text ='', manager=manager)
-windmill1 = pygame.image.load("Windmill1.png")
-windmill.set_image(windmill1)
-
-# timer = pygame_gui.elements.UILabel(relative_rect = pygame.Rect((200, 200), (100,100)), text = f"{time}", manager = manager)
-# clock = pygame.time.Clock()
-# class Timer:
-#     def __init__(self, width, height, screen, ):
-
-class Timer:
-    def __init__(self, x, y, width, height, text , screen):
-        self.rect = pygame.Rect(x, y, width, height)
-        self.text = text
-        self.screen = screen
-    
-    def draw(self):
-        # pygame.draw.rect(screen, self.rect)
-        self.screen.blit(self.surface, self.rect)
-
+class Button:
+	def __init__(self, x, y, image, scale):
+		width = image.get_width()
+		height = image.get_height()
+		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+		self.clicked = False
+		
+	def draw(self, surface):
+		action = False
+		pos = pygame.mouse.get_pos()   
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True   
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+			surface.blit(self.image, (self.rect.x, self.rect.y))    
+		return action
+	
 class Board:
-    def __init__(self, width, height, screen):
-        self.board = pygame.image.load("windmill_board.png")
-        self.rect = self.board.get_rect()
-        self.screen = screen
-        # self.board = pygame.transform.scale(self.board,(600, 600))
-        self.rect.center = (width/2, height/2)
+	def __init__(self, width, height, screen):
+		self.board = pygame.image.load("windmill_board.png")
+		self.rect = self.board.get_rect()
+		self.screen = screen
+		self.rect.center = (width/2, height/2)
 
 
-    def draw(self):
-        surf = self.
-        self.screen.blit(self.board, self.rect)
+	def draw(self):
+		self.screen.blit(self.board, self.rect)
 
-
+windmill_img = pygame.image.load("Windmill1.png")
+windmill_button = Button(100, 200, windmill_img, 0.8)
 board1 = Board(width, height, screen)
-timer1 = Timer(200, 200, width, height, f"{time}", screen)
+
 while True:
-    time_delta = clock.tick(60)/1000.0
-    time = pygame.time.get_ticks()
-    # print(time)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+	time_delta = clock.tick(60)/1000.0
+	time = pygame.time.get_ticks()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			exit()
 
-        if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element == windmill:
-                print('Windmill placed')
-        manager.process_events(event)
-    manager.update(time_delta)
-    manager.update(time)
-    print(time)
-    board1.draw()
-    timer1.draw()
-    manager.draw_ui(screen)
-    # manager.draw_ui(timer)
-    pygame.display.update()
-    # timer.update(time_delta)
-        #game code
 
-    pygame.display.update()
-    # clock.tick(60)
+	# print(time)
+	board1.draw()
+	windmill_button.draw(screen)
+	# timer1.draw()
+
+	pygame.display.update()
+	# timer.update(time_delta)
+		#game code
+
+	pygame.display.update()
+	# clock.tick(60)
